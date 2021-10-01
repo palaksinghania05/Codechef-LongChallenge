99d39dd797b4c6fd518a9680b83c5c3828a17aaf
palaksinghania05 <palak.singhania_cs.ccv19@gla.ac.in>
2021-10-01 17:33:55 +0530

    added september questions

---
+++ src/september/Question2.java
@@ -0,0 +1,32 @@
+//TRAVEL PASS
+
+package september;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int T = sc.nextInt();
+            while(T>0){
+                int N = sc.nextInt();
+                int A = sc.nextInt();
+                int B = sc.nextInt();
+                String travel = sc.next();
+                char[] arr = travel.toCharArray();
+                int time = 0;
+                for (int i = 0; i < N; i++) {
+                    if(arr[i]=='0') {
+                        time += A;
+                    }
+                    else
+                        time += B;
+                }
+                System.out.println(time);
+                T--;
+            }
+        }
+    }
+}
